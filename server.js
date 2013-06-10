d25e3fc1614f66e07e14a806d748de589ac2514b
August Brenner <AugustBrenner@server.fake>
2013-06-10 11:59:51 -0700

    updated server file to parse url path name

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // node.js server file
 
-//require http module
+//required modules
 var http = require("http");
+var url = require("url");
+
 
 // start function to export
 function start() {
 
     // set up the http request/response procedure
     function onRequest(request, response) {
-        console.log("Request received.");
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World");
         response.end();
