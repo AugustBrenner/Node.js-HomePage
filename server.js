3f1514439d2f4b08e9073419ecc181136efe9e02
August Brenner <AugustBrenner@server.fake>
2013-06-10 10:59:43 -0700

    updated server file to demonstrate asynchronous single threaded process

--- server.js
+++ server.js
@@ -4,8 +4,15 @@
 var http = require("http");
 
 // set up the http request/response procedure
-http.createServer(function(request, response) {
+function onRequest(request, response) {
+    console.log("Request received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
-}).listen(8888);
+}
+
+// create the server
+http.createServer(onRequest).listen(8888);
+
+// display confirmation that server started
+console.log("Server has started.");
